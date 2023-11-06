62ce55be0f69c596b9ab03a9ca8153598a449d74
KhaledEllithy <khaledellessy310@gmail.com>
2023-11-07 00:46:48 +0200

    change spinner shape

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AppNavbar from "../components/AppNavbar/AppNavbar";
 import { Outlet } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline, createTheme } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { grey } from "@mui/material/colors";
+import { useDispatch } from "react-redux";
 import { getUserIdFromLocalStorage } from "../helpers/LocalStorageFunctions";
 import { getCartByUserID } from "../store/slices/cartSlice";
 import { getWishListByUserID } from "../store/slices/wishListSlice";
@@ -17,47 +14,6 @@ const Layout = () => {
   //get the cart by user id
   dispatch(getCartByUserID(userId));
   dispatch(getWishListByUserID(userId));
-  // const mode = useSelector((state) => state.mode);
-  // console.log(mode);
-  // const darkTheme = createTheme({
-  //   palette: {
-  //     mode,
-  //     ...(mode === "light"
-  //       ? {
-  //           button: {
-  //             main: grey[900],
-  //             sec: grey[700],
-  //           },
-  //           background: {
-  //             default: "#fff",
-  //             sec: "black",
-  //             card: grey[300],
-  //           },
-  //           text: {
-  //             primary: grey[900],
-  //             secondary: grey[800],
-  //             logoSec: "#fff",
-  //             active: grey[400],
-  //           },
-  //         }
-  //       : {
-  //           button: {
-  //             main: grey[900],
-  //             sec: grey[700],
-  //           },
-  //           background: {
-  //             sec: "#fff",
-  //             card: grey[600],
-  //           },
-  //           text: {
-  //             primary: "#fff",
-  //             secondary: grey[500],
-  //             logoSec: grey[900],
-  //             active: grey[800],
-  //           },
-  //         }),
-  //   },
-  // });
 
   const [isLoading, setIsLoading] = useState(false);
 
--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-// import { Circle } from "react-loading";
-import ReactLoading from "react-loading";
 import "./Spinner.css";
+import { GridLoader, ScaleLoader } from "react-spinners";
 const Spinner = () => {
   return (
     <div className="loader">
-      <ReactLoading
-        type={"balls"}
-        // color={"#000"}
-        height={"100px"}
-        width={"100px"}
-      />
+      <GridLoader />
     </div>
   );
 };
